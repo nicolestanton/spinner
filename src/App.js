44b411e5b26efc82a37b6938d714bb169efdbda3
Nicole Stanton <n.stanton14@ymail.com>
2021-05-19 19:27:38 +0100

    Add conditionals to view for states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Button from "./Components/Button";
-import { Spinner, StaticSpinner } from "./Components/Spinner";
+import { Spinner, StaticSpinner, CompletedSpinner } from "./Components/Spinner";
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [percentage, setPercentage] = useState(0);
+  const [spinnerComplete, setSpinnerComplete] = useState(false);
 
   function startSpinner() {
     setLoading(true);
+    setSpinnerComplete(false);
     console.log("spinner started");
   }
 
@@ -19,16 +21,20 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (percentage >= 100) {
+      if (percentage === 100) {
+        console.log("percentage", percentage);
+
         setPercentage(100);
         setLoading(false);
+        return;
       } else {
         setPercentage(percentage + 1);
       }
     }, 100);
 
-    if (loading === false) {
+    if (loading === false && percentage === 100) {
       setPercentage(0);
+      setSpinnerComplete(true);
     }
 
     return () => {
@@ -36,16 +42,31 @@ function App() {
     };
   });
 
+  if (spinnerComplete === true) {
+    return (
+      <div className="app">
+        <div className="spinner-container">
+          <h3>Completed!</h3>
+          <Spinner spinnerValue="100" className={"spinner-ring complete"} />
+          <Button onClick={startSpinner} text={"Restart"} />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="app">
-      {loading === true ? (
+      {loading === true && spinnerComplete === false ? (
         <div className="spinner-container active ">
-          <Spinner spinnerValue={percentage} />
+          <Spinner
+            spinnerValue={percentage}
+            className={"spinner-ring active"}
+          />
           <Button onClick={stopSpinner} text={"Stop"} />
         </div>
       ) : (
         <div className="spinner-container">
-          <StaticSpinner spinnerValue="0" />
+          <Spinner spinnerValue="0" className={"spinner-ring"} />
           <Button onClick={startSpinner} text={"Start"} />
         </div>
       )}
--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,24 +1,32 @@
 import React from "react";
 
 export function Spinner(props) {
-  const { spinnerValue } = props;
+  const { spinnerValue, className } = props;
   return (
     <div className="spinner">
       <span className="spinner-value">{spinnerValue}%</span>
-      <div className="spinner-ring active"></div>
+      <div className={className}></div>
     </div>
   );
 }
 
-export function StaticSpinner(props) {
-  const { spinnerValue } = props;
-  return (
-    <div className="spinner static">
-      <span className="spinner-value">0%</span>
-      <div className="spinner-ring"></div>
-    </div>
-  );
-}
+// export function StaticSpinner() {
+//   return (
+//     <div className="spinner static">
+//       <span className="spinner-value">0%</span>
+//       <div className="spinner-ring"></div>
+//     </div>
+//   );
+// }
+
+// export function CompletedSpinner() {
+//   return (
+//     <div className="spinner static">
+//       <span className="spinner-value">100%</span>
+//       <div className="spinner-ring completed"></div>
+//     </div>
+//   );
+// }
 
 // className={
 //     spinnerValue === 100 ? "spinner-ring completed" : "spinner-ring"
